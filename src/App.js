688a4dbead6fa3b2990ca834a5387988452fd23b
Chitalu Mumba <49457879+chitalu7@users.noreply.github.com>
2021-12-01 12:23:01 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Container, Card, CardContent, makeStyles, Grid } from '@material-ui/core';
+import { Container, Card, CardContent, makeStyles, Grid, ButtonBase, Button } from '@material-ui/core';
 import QrReader from 'react-qr-reader';
 
 function App() {
@@ -23,17 +23,28 @@ function App() {
             <Card>
                 <h2 className={classes.title}>Easy Entry Store QR Code ReactJS</h2>
                 <CardContent>
+                    <Grid container spacing={2}>
                     <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
                         <h3>QR Webcam Scanner</h3>
                         <QrReader
                             delay={300}
-                            style={{ width: '100%' }}
+                            style={{ width: '80%' }}
                             onError={handleErrorWebCam}
                             onScan={handleScanWebCam}
                         />
                         <h3>Scanned by WebCam Code: {scanResultWebCam}</h3>
                     </Grid>
-
+                    <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                        <h3>Customer Information</h3>
+                        <span><h4>Customer Name: John Smith </h4></span>
+                        <h4>Vaccine 1: Pz </h4>
+                        <h4>Vaccine 2: Pz</h4>
+                        <h4>Vaccine 3: Pz</h4>
+                        <h4>Booster Shot: Pending</h4>
+                        <h4>Status: Vaccinated</h4>
+                        <Button>Clear Form</Button>
+                    </Grid>
+</Grid>
                 </CardContent>
             </Card>
         </Container>
